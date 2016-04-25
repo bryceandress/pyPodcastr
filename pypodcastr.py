f343e8137448ceb91e86c7efec0ba4b99cb4a559
Bryce Andress <andressbryce@gmail.com>
2016-04-25 19:31:48 -0400

    deleted functions

--- pypodcastr.py
+++ pypodcastr.py
@@ -7,65 +7,58 @@ import sys
 podcastdir = os.getenv("HOME")+"/Podcasts/"
 
 #Add a podcast
-def addPodcasts():
-  try:
-    if sys.argv[1] == "--add":
-      podcasts.append(str(sys.argv[2])+"\n")
-      f = open(".podcasts.pod", "w")
-      for line in podcasts:
-        f.write(line)
-      f.close()
+try:
+  if sys.argv[1] == "--add":
+    podcasts.append(str(sys.argv[2])+"\n")
+    f = open(".podcasts.pod", "w")
+    for line in podcasts:
+      f.write(line)
+    f.close()
     
-      print("Starting")
-  except:
     print("Starting")
+except:
+  print("Starting")
 
 #try to read what podcasts to download
-def subscribedPodcasts():
-  try:
-    f = open(".podcasts.pod", "r")
-    podcasts = f.readlines()
-    f.close()
-  except:
-    print("Please add some podcasts to catch")
-    sys.exit()
+try:
+  f = open(".podcasts.pod", "r")
+  podcasts = f.readlines()
+  f.close()
+except:
+  print("Please add some podcasts to catch")
+  sys.exit()
 
 #For all the podcasts download the latest
 #if the latest is not in the .lastdownload file
 #if it is just skip
-def downloadPodcasts():
-  for podcast in podcasts:
-    feed = feedparser.parse(podcast)
+for podcast in podcasts:
+  feed = feedparser.parse(podcast)
 
-    #Make sure there is a podcast folder
-    if not(os.path.exists(podcastdir)):
-      os.mkdir(podcastdir)
+  #Make sure there is a podcast folder
+  if not(os.path.exists(podcastdir)):
+    os.mkdir(podcastdir)
 
-    #Make correct podcast directories
-    if not (os.path.exists(podcastdir + feed.feed.title + "/")):
-      os.mkdir(podcastdir + feed.feed.title + "/")
+  #Make correct podcast directories
+  if not (os.path.exists(podcastdir + feed.feed.title + "/")):
+    os.mkdir(podcastdir + feed.feed.title + "/")
   
-    #Try to read in last downloaded files
-    try:
-      f = open(".lastdownloaded", "r")
-      lastdownloaded = f.readlines()
+  #Try to read in last downloaded files
+  try:
+    f = open(".lastdownloaded", "r")
+    lastdownloaded = f.readlines()
+    f.close()
+  except:
+    lastdownloaded = []
+  
+  for key in feed["entries"]:
+    if (key["title"] + "\n") in lastdownloaded:
+      print(key["title"] + " already downloaded...skipping")
+    else:
+      print("Downloading: ", key["title"])
+      urllib.request.urlretrieve(key["link"], podcastdir + feed.feed.title + "/" + key["title"] + ".mp3")
+      f = open(".lastdownloaded", "a")
+      f.write(key["title"] + "\n")
       f.close()
-    except:
-      lastdownloaded = []
-  
-    for key in feed["entries"]:
-      if (key["title"] + "\n") in lastdownloaded:
-        print(key["title"] + " already downloaded...skipping")
-      else:
-        print("Downloading: ", key["title"])
-        urllib.request.urlretrieve(key["link"], podcastdir + feed.feed.title + "/" + key["title"] + ".mp3")
-        f = open(".lastdownloaded", "a")
-        f.write(key["title"] + "\n")
-        f.close()
-      break
-
-addPodcasts()
-subscribedPodcasts()
-downloadPodcasts()
+    break
 print("Podcasts are up to date")
 
